fff7eb178ceca6742e9b0fc760fec985e76e7fbb
Sam Sharma <samsharma@Sams-MacBook-Pro.local>
2020-02-20 04:02:18 -0800

    Updates

---
+++ precache-manifest.770aa4bb5275d8a464b30bd24d1bcc39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9340011b8d96308cfa5eed76b6440c21",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "7f3d298d2dd3a7447ea0",
+    "url": "/weather-app/static/css/main.37b90dcf.chunk.css"
+  },
+  {
+    "revision": "cd51997c320fd8dff5e8",
+    "url": "/weather-app/static/js/2.6c01e69c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather-app/static/js/2.6c01e69c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f3d298d2dd3a7447ea0",
+    "url": "/weather-app/static/js/main.e9309daf.chunk.js"
+  },
+  {
+    "revision": "2ab674f0efc76b1babef",
+    "url": "/weather-app/static/js/runtime-main.8a1041ad.js"
+  },
+  {
+    "revision": "d615bc83d356edba89e9cf66fd0ad922",
+    "url": "/weather-app/static/media/cold.d615bc83.jpeg"
+  },
+  {
+    "revision": "69c144bfa6f797b4b0800c108f5e2b3d",
+    "url": "/weather-app/static/media/hot.69c144bf.jpeg"
+  }
+]);
